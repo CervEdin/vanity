235371a3f2286da4ab3a6a8d77c15f35d7847318
Erik Cervin Edin <erik@cervined.in>
2021-12-13 18:13:21 +0100

    Commit a random amount

--- work.sh
+++ work.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-for Y in `seq 2001 2010`; do
+for Y in `seq 2002 2010`; do
   for D in `seq 0 364`; do
-    for i in `seq 0 100`; do
+    N=`tr -cd 0-9 </dev/urandom | head -c 2`
+    for i in `seq 1 $N`; do
       DATE=`date -I -d "$Y-01-01 +$D days"`
       git commit --allow-empty --allow-empty-message -m '' --date $DATE;
     done
